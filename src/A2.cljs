fb009160206a49f537ba1980a94f2b09474da8f0
ma721kup <kupferschmidmathias@gmail.com>
2018-10-22 13:57:13 +0200

    A2

---
+++ src/A2.cljs
@@ -0,0 +1,21 @@
+(ns A2)
+
+(defn facNonRec [n](
+
+                     apply *(range 1 (+ n 1))
+ ))
+
+(defn facRec [n](
+                   if (= n 1)
+                   1
+                   (* n (facRec(- n 1)))
+                   ))
+
+(defn facLR [n](
+  loop [x n prod 1](if (not (> x 1))
+                           prod
+                  (recur (- x 1) (* x prod)))
+                 ))
+
+
+(println (facLR 5))
