ad330ae2edef30fbc53e99e9d1405df019aea5cd
ma721kup <kupferschmidmathias@gmail.com>
2018-10-25 15:53:20 +0200

    A4 finished

--- src/A4.cljs
+++ src/A4.cljs
@@ -1,9 +1,11 @@
 (ns A4)
 
+
+
 (defn brackets [l](
-                    if(> (count l) 1)
-                    (conj (conj '() (brackets (rest l)) (first l)) )
-                    (conj '()(first l))
+                    if(> (count l) 0)
+                    (conj (brackets (rest l)) (list(first l)) )
+                    ()
                     ))
 
 (println(brackets '(1 2 3 4)))
